1967d795f0346a239fb4eff89a10ec21da618836
dudu.com <hassanwashim51@gmail.com>
2026-01-18 19:32:04 +0530

    Add createHeart function to generate hearts

--- script.js
+++ script.js
@@ -68,3 +68,19 @@ function createHeart() {
 }
 
 setInterval(createHeart, 300);
+function createHeart() {
+  const heart = document.createElement("div");
+  heart.className = "heart";
+  heart.innerHTML = "❤️";
+
+  heart.style.left = Math.random() * 100 + "vw";
+  heart.style.animationDuration = (Math.random() * 3 + 3) + "s";
+
+  document.body.appendChild(heart);
+
+  setTimeout(() => {
+    heart.remove();
+  }, 6000);
+}
+
+setInterval(createHeart, 300);
